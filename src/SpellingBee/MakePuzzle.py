27033b33905100f815f61e45bc04668e8295b29e
Frannyspag24 <francescospag24@gmail.com>
2023-02-07 15:59:14 -0500

    update guess

--- src/SpellingBee/MakePuzzle.py
+++ src/SpellingBee/MakePuzzle.py
@@ -87,6 +87,8 @@ def checkDataBase(baseWord):
 def choseKeyLetter(uniqueLetters):
     return random.choice(uniqueLetters)
 
+#params: letters is a list of the letters
+#shuffles letters but keeps the center letter at the front of the list
 def shuffle(letters):
         
     mainLetter = letters[0] #saving the main letter for before the shuffle
@@ -101,29 +103,34 @@ def shuffle(letters):
                 
     return letters
 
+#params: puzzle object, input that the user gave
+#checks the database for valid words, already found words and words that do not exist
 def guess(puzzle, input):
     
     conn = sqlite3.connect('src/SpellingBee/wordDict.db')
     cursor = conn.cursor()
         
-    #check for every case in the user's guess to give points or have them input again
-    if input in puzzle.showAllWordList():
-        if input in puzzle.showFoundWords():
+    #check for every case in the user's guess to give points or output error
+    if input in puzzle.showAllWordList(): #checks words in the word list to see if it is valid for the puzzle
+        if input in puzzle.showFoundWords(): #check if it is already found
             print("Already Found")
         else:
+            #query the database to see how many points to give
             query = "select wordScore from dictionary where fullWord = '" + [input] + "';"
             cursor.execute(query)
             puzzle.updatePoints(cursor.fetchone()[0])
-    elif len(input) < 4:
+    elif len(input) < 4: #if the word is not in the list check the size
         print("Too short")
     else:
+        #query the database to see if it is a word at all
         query1 = "select uniqueLetters from dictionary where fullWord = '" + [input] + "';"
         response = cursor.execute(query1)
         if response == None:
             print("Not a word in word list")
-        elif set(response[0]).issubset(set(puzzle.showUniqueLetters())):
+        elif set(response[0]).issubset(set(puzzle.showUniqueLetters())): #check if the letters contain the center letter
             print("Missing center letter")
         else:
+            #must be letters not in the puzzle in this case
             print("Bad letters")
             
     conn.commit()
