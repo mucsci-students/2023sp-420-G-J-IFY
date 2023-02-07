d23b0e6e43b1815cffaf4cd6e0358edaef1c0c02
Jacob Lovegren <122911888+JMLovegren@users.noreply.github.com>
2023-02-07 17:12:20 -0500

    Made some tweaks to guess, still have an issue Puzzle.updateFoundWords is giving me guff

--- src/SpellingBee/saveState.py
+++ src/SpellingBee/saveState.py
@@ -187,8 +187,37 @@ class Puzzle:
 #puzzPuzz.wordListStorage()
 #print(puzzPuzz.showAllWords())
 
-#test Code
-#warlock = acklorw
-#newPuzz = Puzzle('a', 'acklorw')
-#newPuzz.wordListStorage()
-#print(newPuzz.showAllWords())
+"""
+newPuzz = Puzzle('a', 'acklorw')
+newPuzz.wordListStorage()
+print(newPuzz.showAllWords())
+#correct guess
+MakePuzzle.guess(newPuzz, 'warlock')
+print(newPuzz.showScore())
+print(newPuzz.showFoundWords())
+#bad letter
+MakePuzzle.guess(newPuzz, 'wart')
+print(newPuzz.showScore())
+#too short
+MakePuzzle.guess(newPuzz, 'war')
+print(newPuzz.showScore())
+#missing key
+MakePuzzle.guess(newPuzz, 'cork')
+print(newPuzz.showScore())
+#double guess
+MakePuzzle.guess(newPuzz, 'warlock')
+print(newPuzz.showScore())
+print(newPuzz.showFoundWords())
+#double guess
+MakePuzzle.guess(newPuzz, 'warlock')
+print(newPuzz.showScore())
+print(newPuzz.showFoundWords())
+#double guess
+MakePuzzle.guess(newPuzz, 'warlock')
+print(newPuzz.showScore())
+print(newPuzz.showFoundWords())
+#double guess
+MakePuzzle.guess(newPuzz, 'warlock')
+print(newPuzz.showScore())
+print(newPuzz.showFoundWords())
+"""
