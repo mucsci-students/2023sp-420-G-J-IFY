33ba94b6f371048ebdbdfebe81d593d3f85b0654
Isaak Weidman <irweidma@millersville.edu>
2023-02-08 17:22:40 -0500

    Minor tweaks, finished __main__.py

--- __main__.py
+++ __main__.py
@@ -1,24 +1,33 @@
 from src.SpellingBee import CLI
 from src.SpellingBee import CommandHandler
-from src.SpellingBee import MakePuzzle
 import os
 
 CLI.drawTextBox(['Welcome to Spelling Bee! \ Presented by G(J)IFY',
     'To start a new game, type \"!new\". \ To load a previous save, type \"!load\"'], 40, '^')
 
 # TODO, restrict input to JUST !new, !load, and !exit
-puzzle = CommandHandler.parse(input('> '))
 
-os.system('clear')
+input = input('> ')
+validIn = False
 
-while 'True':
+while not validIn:
+    match input:
+        case '!new':
+            puzzle = CommandHandler.newPuzzle()
+            validIn = True
+        case '!load':
+            puzzle = CommandHandler.loadGame()
+            validIn = True
+        case '!exit':
+            quit()
+        case _:
+            os.system('clear')
+            CLI.drawTextBox(['Command Not recognized. Please try again.',
+                    'To start a new game, type \"!new\". \ To load a previous save, type \"!load\"'], 40, '^')
+            input = input('> ')
 
-    # The following is just a placeholder, mainly just to showcase the idea.
-    tier1 = 'Welcome to Spelling Bee! \ Presented by G(J)IFY'
-    tier2 = 'Level: \ {lvl} {pBar} \ ({num} more to \'{nxtLvl}\')'.format(lvl = 'Novice', pBar = CLI.drawProgressBar(20, 0.35), num = 8, nxtLvl = 'Okay')
-    tier3 = 'Discovered Words: \ {wrds}'.format(wrds = CLI.drawList(['ICING', 'ACING', 'AGING']))
-    tier4 = CLI.drawPuzzle(['I', 'O', 'A', 'N', 'G', 'X', 'C'])
-    tier5 = 'Enter your guess, or type \'!help\' for a list of commands.'
-    CLI.drawTextBox([tier1, tier2, tier3, tier4, tier5], 40, '^')
+while True:
 
+    os.system('clear')
+    CLI.drawGameBox(puzzle)
     CommandHandler.parse(input('> '), puzzle)
