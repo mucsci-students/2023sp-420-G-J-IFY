4c8eb2f3a8bef9da7cd38776bbdc86b2c6e93b42
Yah'hymbey Baruti Ali-Bey <yahhymbey.barutialibey@yahoo.com>
2023-02-20 19:59:48 -0500

    Style Updates to Generate Max Score

--- src/model/generateMaxScore.py
+++ src/model/generateMaxScore.py
@@ -1,5 +1,14 @@
-#Jacob Lovegren 2/2/23
-#Generate max score from wordlist
+################################################################################
+# generateMaxScore.py
+# Author: Jacob Lovegren
+# Date of Creation: 2-2-23
+#   Generate max score from wordlist
+# Multi-line description of what a module does.
+#
+# (Global, public) functions:
+#   findMaxScore(listList : list) -> int
+#       - connects to the database to generate max score
+################################################################################
 
 import sqlite3
 
@@ -7,10 +16,22 @@ import sqlite3
 #findMaxScore - this functions takes a list of words 
 #for a game, quereies the DB for all words in the game,
 #and adds together the total possible points for the given pangram
-#@PARAM listList, a list of strings containing all words in DB
-#   for given pangram 
-#@RETURN maxScore, the total possible score for a starting word
-def findMaxScore(listList):
+################################################################################
+# findMaxScore(listList: list) -> int
+#
+# DESCRIPTION:
+#   this functions takes a list of words  for a game, quereies the DB for all words in the game,
+#   and adds together the total possible points for the given pangram
+#
+# PARAMETERS:
+#  listList : list
+#   a list of strings containing all words in DB for given pangram 
+#
+# RETURNS:
+#  maxScore
+#   int type
+################################################################################
+def findMaxScore(listList: list):
     #connect to DB
     conn = sqlite3.connect('wordDict.db')
     cursor = conn.cursor()
