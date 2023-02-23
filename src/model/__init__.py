159ccee1c705b68e61ca92378a24736ee11e806d
Jacob Lovegren <122911888+JMLovegren@users.noreply.github.com>
2023-02-23 09:42:36 -0500

    small fixes after merging modelRefactor

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append('src/model/saveState.py')
 
-from .generateMaxScore import *
-from .generateSubset import *
 from .MakePuzzle import *
-from .saveState import *
+from .puzzle import *
 from .StateStorage import *
