4dc492d3aecd5991d9c8a73a6b70ba91a8363a52
JMLovegren <jmlovegr@millersville.edu>
2023-03-01 00:38:16 -0500

    threw together output class

---
+++ src/model/output.py
@@ -0,0 +1,10 @@
+class Output:
+
+    def __init__(self):
+        self.field = ""
+
+    def setField(self, inStr):
+        self.field = inStr
+
+    def getField(self):
+        return self.field
