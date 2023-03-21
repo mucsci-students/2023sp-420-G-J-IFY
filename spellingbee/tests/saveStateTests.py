c5fc6ec9b171f4de02722c71b327ac3a05979f3e
Gmzakros <gmzakros@millersville.edu>
2023-03-21 13:16:37 -0400

    converted tests to use pytest

--- spellingbee/tests/saveStateTests.py
+++ spellingbee/tests/saveStateTests.py
@@ -41,7 +41,6 @@ def testEmptyPuzzleAllWords(puzzleFixture):
 
 def testEmptyPuzzleRank(puzzleFixture):
     assert(puzzleFixture.rank == ' ')
-puzzleFixture
 def testGetKeyLetter(puzzleFixture):
     assert(puzzleFixture.getKeyLetter() == puzzleFixture.keyLett)
         
@@ -74,61 +73,75 @@ def testGetRank(puzzleFixture):
     assert(puzzleFixture.getRank() == puzzleFixture.rank)
 
     # testUpdateFoundWords
-def testUpdateFoundWordsGood()
+def testUpdateFoundWordsGood(puzzleFixture):
     puzzleFixture.updateFoundWords('warlock')
     puzzleFixture.updateFoundWords('wrack')
     puzzleFixture.updateFoundWords('wallaroo')
     assert(puzzleFixture.foundWordList == ['warlock', 'wrack', 'wallaroo'])
-       
 
     #test UpdateScore
+def testUpdateScore(puzzleFixture):
     puzzleFixture.updateScore(10)
     assert(puzzleFixture.score == 10)
+
+def testUpdateScore2(puzzleFixture):
+    puzzleFixture.updateScore(10)
     puzzleFixture.updateScore(30)
     assert(puzzleFixture.score == 40)
 
     # test SetKeyLetter
+def testSetKeyLetter(puzzleFixture):
     puzzleFixture.setKeyLetter('b')
     assert(puzzleFixture.keyLett == 'b')
 
     # test SetUniqueLetters
+def testSetKeyLetter(puzzleFixture):
     puzzleFixture.setUniqueLetters('barnical')
     assert(puzzleFixture.uniqueLett == 'barnical')
         
     # test SetShuffleLetters
+def testShuffleLetters(puzzleFixture):
     puzzleFixture.setShuffleLetters('barnical')
     assert(puzzleFixture.shuffleLett == 'barnical')
         
     # test SetScore
+def testSetScore(puzzleFixture):
     puzzleFixture.setScore(100)
     assert(puzzleFixture.score == 100)
 
     # test SetMaxScore
+def testSetScore(puzzleFixture):
     puzzleFixture.setMaxScore(200)
     assert(puzzleFixture.maxScore == 200)
 
     # test SetFoundWords
+def testSetFoundWords(puzzleFixture):
     list = ["warlock", "wrack", "alcool", "arrack", "wallaroo"]
     puzzleFixture.setFoundWords(list)
     assert(puzzleFixture.getFoundWords() == ["warlock", "wrack", "alcool", "arrack", "wallaroo"])
        
     # test SetAllWordList
+def testSetAllWordList(puzzleFixture):
     puzzleFixture.setAllWordList(["warlock", "wrack", "alcool", "arrack", "wallaroo", "cloacal", "corolla", "wallow", "corral"])
     assert(puzzleFixture.allWordList == ["warlock", "wrack", "alcool", "arrack", "wallaroo", "cloacal", "corolla", "wallow", "corral"])
        
     # test  SetRank
+def testSetRank(puzzleFixture):
     puzzleFixture.setRank('Queen Bee')
     assert(puzzleFixture.rank == 'Queen Bee')
       
     # test UpdateRank
+def testUpdateRank(puzzleFixture):
+    puzzleFixture.setMaxScore(200)
+    puzzleFixture.setScore(100)
     puzzleFixture.updateRank()
     assert(puzzleFixture.rank == "Great")
        
     # test ShuffleChars 
+def testShuffleChars(puzzleFixture):
     puzzleFixture.shuffleChars()
     assert( not puzzleFixture.shuffleLett == puzzleFixture.uniqueLett)
        
 
-if __name__ == '__main__':
-    unittest.main()
+
     
