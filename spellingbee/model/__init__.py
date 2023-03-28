2f319eb61a8ecf5acb1012e8df81b13b2106bf57
Frannyspag24 <francescospag24@gmail.com>
2023-03-28 13:45:27 -0400

    init change

--- spellingbee/model/__init__.py
+++ spellingbee/model/__init__.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.append('spellingbee/model/saveState.py')
+sys.path.append('spellingbee/model/puzzle.py')
 
-#from .MakePuzzle import *
+from .MakePuzzle import *
 from .puzzle import *
 from .StateStorage import *
 from .dbFixer import *
