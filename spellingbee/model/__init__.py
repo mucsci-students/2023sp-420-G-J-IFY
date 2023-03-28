b2ed360f61c7b3bff6cd9543cbf7ed66c44964fc
Frannyspag24 <francescospag24@gmail.com>
2023-03-28 13:42:06 -0400

    test

--- spellingbee/model/__init__.py
+++ spellingbee/model/__init__.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('spellingbee/model/saveState.py')
 
-from .MakePuzzle import *
+#from .MakePuzzle import *
 from .puzzle import *
 from .StateStorage import *
 from .dbFixer import *
