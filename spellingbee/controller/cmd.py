af676388d7a63b6d4c1f1fc974636b5b26e6d762
Gmzakros <gmzakros@millersville.edu>
2023-04-12 15:46:58 -0400

    fix good?

--- spellingbee/controller/cmd.py
+++ spellingbee/controller/cmd.py
@@ -109,9 +109,11 @@ class SaveGame(Command):
             onlyPuzz=self._onlyPuzz
         )
 
-    def executeCLI(self) -> None:
+    def executeCLIPuzzle(self) -> None:
         StateStorage.savePuzzle(self._puzzle, self._fileName)
 
+    def executeCLICurrent (self) -> None:
+        StateStorage.saveCurrent(self._puzzle, self._fileName)
 ###############################################################################
 # class LoadGame(Command)
 #
