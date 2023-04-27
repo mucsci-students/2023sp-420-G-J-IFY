8a8ca612d523bc2c0b86360263acacb7623c5089
Frannyspag24 <francescospag24@gmail.com>
2023-04-27 12:53:36 -0400

    Fixing quick cli crash

--- spellingbee/controller/cController/CLIAdapter.py
+++ spellingbee/controller/cController/CLIAdapter.py
@@ -571,7 +571,7 @@ class CLI_A():
             case 'N':
                 return flag
             case _:
-                self.outty.setField('Input Invalid')
+                outty.setField('Input Invalid')
                 # Recursively calls until valid input provided.
                 self.checkEncrypt()
 
